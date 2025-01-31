a4ccc0de3db06b805a757b3d2cff527a000bb650
NathanW0909 <nawong8@s.sfusd.edu>
2025-01-30 21:31:12 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,34 @@
-
-
 public class Main {
- 
-	public static void main(final String[] args) {
-		PalindromeChecker sim = new PalindromeChecker();
-        	sim.tester();
-	}
-	
-
+    public static void main(String[] args) {
+        PalindromeChecker sim = new PalindromeChecker();
+        sim.tester();
+    }
 }
 
+class PalindromeChecker {
+    public String reverse(String s) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed.append(s.charAt(i));
+        }
+        return reversed.toString();
+    }
+
+
+    public void tester() {
+        String[] tests = {
+            "test",
+            "rotator",
+            "rewriter",
+            "nurses run",
+            "Madam, I'm Adam!",
+            "A Man! A Plan! A Canal! Panama!"
+        };
+
+        System.out.println("There are " + tests.length + " lines");
+
+        for (String test : tests) {
+            System.out.println(test + (isPalindrome(test) ? " is a palindrome." : " is NOT a palindrome."));
+        }
+    }
+}
