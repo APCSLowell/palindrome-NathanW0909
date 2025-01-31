b5d7aeab98fa019674524e5285ca725aa75d6f2b
NathanW0909 <nawong8@s.sfusd.edu>
2025-01-30 22:20:02 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        PalindromeChecker sim = new PalindromeChecker();
-        sim.tester();
-    }
-}
-
-class PalindromeChecker {
-    public String reverse(String s) {
+public class PalindromeChecker {
+    public static String reverse(String s) {
         return new StringBuilder(s).reverse().toString();
     }
 
-    public boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         return s.equals(reverse(s));
     }
 
-    public void tester() {
-        checkPalindrome("test");
-        checkPalindrome("rotator");
-        checkPalindrome("rewriter");
-        checkPalindrome("nurses run");
-        checkPalindrome("Madam, I'm Adam!");
-        checkPalindrome("A Man! A Plan! A Canal! Panama!");
+    public static void main(String[] args) {
+        testPalindrome("test");
+        testPalindrome("rotator");
+        testPalindrome("rewriter");
+        testPalindrome("nurses run");
+        testPalindrome("Madam, I'm Adam!");
+        testPalindrome("A Man! A Plan! A Canal! Panama!");
     }
 
-    public void checkPalindrome(String s) {
-        System.out.println(s + (isPalindrome(s) ? " is a palindrome." : " is NOT a palindrome."));
+    public static void testPalindrome(String s) {
+        System.out.println(s + " -> " + (isPalindrome(s) ? "Palindrome" : "Not a palindrome"));
     }
 }
