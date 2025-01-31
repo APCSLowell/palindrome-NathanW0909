2f2923c22ed31f61215eb482790b0d4ba05fedfb
NathanW0909 <nawong8@s.sfusd.edu>
2025-01-30 22:54:13 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class PalindromeChecker {
-    public static String reverse(String s) {
-        return new StringBuilder(s).reverse().toString();
+    public void tester() {
+        String[] lines = new String[6];
+        try {
+            File myFile = new File("palindromes.txt");
+            Scanner myReader = new Scanner(myFile);
+            int counter = 0;
+            while (myReader.hasNextLine()) {
+                lines[counter] = myReader.nextLine();
+                counter++;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("there are " + lines.length + " lines");
+        for (String line : lines) {
+            if (palindrome(line)) {
+                System.out.println(line + " IS a palindrome.");
+            } else {
+                System.out.println(line + " is NOT a palindrome.");
+            }
+        }
     }
 
-    public static boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        return s.equals(reverse(s));
+    public boolean palindrome(String word) {
+        String cleaned = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        return cleaned.equals(reverse(cleaned));
+    }
+
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 
     public static void main(String[] args) {
-        testPalindrome("test");
-        testPalindrome("rotator");
-        testPalindrome("rewriter");
-        testPalindrome("nurses run");
-        testPalindrome("Madam, I'm Adam!");
-        testPalindrome("A Man! A Plan! A Canal! Panama!");
-    }
-
-    public static void testPalindrome(String s) {
-        System.out.println(s + " -> " + (isPalindrome(s) ? "Palindrome" : "Not a palindrome"));
+        new PalindromeChecker().tester();
     }
 }
